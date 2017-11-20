36c28c3562b868791f2ba646a07eabe93b17eddb
Frank Delaguila <frank.delaguila@gmail.com>
2017-11-20 14:40:33 -0700

    Remove node modules

---
+++ exercises/matrix-debug/app.js
@@ -0,0 +1,12 @@
+var matrix = [];
+rows = 3;
+columns = 3;
+
+for (var i = 0; i < rows; i++){
+  matrix.push([]);
+  for (var j = 0; j < columns; j++){
+    matrix[i].push(0);
+  }
+}
+
+console.log(matrix);
