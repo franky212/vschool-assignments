ffb6bcd7dad79fb5b3ba2c18edcb654d3392655e
Frank Delaguila <frank.delaguila@gmail.com>
2018-01-16 13:11:01 -0700

    Tried fixing banner on mobile screens. Apparently it already works my phone is just stupid.

--- projects/personal-portfolio/src/components/Banner.js
+++ projects/personal-portfolio/src/components/Banner.js
@@ -3,7 +3,7 @@ import $ from "jquery";
 
 export default function Banner() {
   return (
-    <div id="banner" className="banner">
+    <div id="banner" className="banner" role="banner" aria-label="Frank Delaguila Web Designer and Developer">
       <div className="container">
       <div className="banner-text">
           <h1>Web Designer and Developer</h1>
@@ -11,6 +11,5 @@ export default function Banner() {
         </div>
       </div>
     </div>
-
   )
 }
--- projects/personal-portfolio/src/components/Work.js
+++ projects/personal-portfolio/src/components/Work.js
@@ -59,7 +59,7 @@ export default class Portfolio extends Component {
                 </Modal.Header>
                 <Modal.Body>
                   <img className="modal-img" alt="Example 1 for Omnifood by Frank Delaguila" src={require("../img/work1.jpg")} />
-                  <p>I wanted to create portfolio work that I could be proud of, so I decided to make a small one page fictional business website. The site is fully responsive, and comes with a mobile menu as well. The premise is a food delivery site that highlights their mobile application, locations, reviews, pricing, and a way to get in contact with them. You can view it here: <a href="http://frankdelaguila-omnifood.surge.sh/">Omnifood Example</a></p>
+                  <p>I wanted to create portfolio work that I could be proud of, so I decided to make a small one page fictional business website. The site is fully responsive, and comes with a mobile menu as well. The premise is a food delivery site that highlights their mobile application, locations, reviews, pricing, and a way to get in contact with them. You can view it here: <a rel="noopener noreferrer" target="_blank" href="http://frankdelaguila-omnifood.surge.sh/">Omnifood Example</a></p>
                 </Modal.Body>
                 <Modal.Footer>
                   <Button onClick={() => this.setState({showModal1: false})}>Close</Button>
@@ -80,7 +80,7 @@ export default class Portfolio extends Component {
                 </Modal.Header>
                 <Modal.Body>
                   <img className="modal-img" alt="Example 2 for What's Going On by Frank Delaguila" src={require("../img/work2.jpg")} />
-                  <p>I’ve been working on a React based app called What’s Going On in my spare time as project work. The premise is having an interactive way to view events that are near a user as well as business' being able to post events to the map for users to see. You can check it out here: <a href="whats-going-on.surge.sh">What's Going On Example</a></p>
+                  <p>I’ve been working on a React based app called What’s Going On in my spare time as project work. The premise is having an interactive way to view events that are near a user as well as business' being able to post events to the map for users to see. You can check it out here: <a rel="noopener noreferrer" target="_blank" href="http://whats-going-on.surge.sh">What's Going On Example</a></p>
                 </Modal.Body>
                 <Modal.Footer>
                   <Button onClick={() => this.setState({showModal2: false})}>Close</Button>
@@ -101,7 +101,7 @@ export default class Portfolio extends Component {
                 </Modal.Header>
                 <Modal.Body>
                   <img className="modal-img" alt="Example 3 for Cyberpunk 2047 by Frank Delaguila" src={require("../img/cyberpunk2047.png")} />
-                  <p>This was a group full-stack project <a href="whats-going-on.surge.sh">What's Going On Example</a></p>
+                  <p>This was a group full-stack project <a rel="noopener noreferrer" target="_blank" href="#">LINK COMING SOON</a></p>
                 </Modal.Body>
                 <Modal.Footer>
                   <Button onClick={() => this.setState({showModal3: false})}>Close</Button>
